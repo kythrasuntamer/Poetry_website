ab60d7508158a0e42d2d4bf20a87cd01e94157dc
Paul Michael Tidwell <121031213+kythrasuntamer@users.noreply.github.com>
2026-01-12 23:17:14 -0800

    Create auth.rs

---
+++ auth.rs
@@ -0,0 +1,19 @@
+use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use argon2::password_hash::{SaltString, rand_core::OsRng};
+
+pub fn hash_password(password: &str) -> anyhow::Result<String> {
+    let salt = SaltString::generate(&mut OsRng);
+    let hash = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)
+        .map_err(|e| anyhow::anyhow!(e.to_string()))?
+        .to_string();
+    Ok(hash)
+}
+
+pub fn verify_password(password: &str, password_hash: &str) -> anyhow::Result<bool> {
+    let parsed = PasswordHash::new(password_hash)
+        .map_err(|e| anyhow::anyhow!(e.to_string()))?;
+    Ok(Argon2::default()
+        .verify_password(password.as_bytes(), &parsed)
+        .is_ok())
+}
